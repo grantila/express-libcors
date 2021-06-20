de7251929f2cf836c12c5dbf16b7db2c1a64c411
Gustaf Räntilä <g.rantila@gmail.com>
2021-06-20 22:58:02 +0200

    feat(core): using GitHub Actions instead of Travis, requiring Node 12
    BREAKING CHANGE: Requires Node 12

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testMatch: [ '<rootDir>/lib/**/*.spec.ts' ],
+	testMatch: [ '<rootDir>/lib/**/*.test.ts' ],
 	collectCoverageFrom: [ '<rootDir>/lib/**' ],
 	coverageReporters: [ 'lcov', 'text', 'html' ],
 };
